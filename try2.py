dce0f856aaae5a93fe4e74454ec6138d658cc187
anskong <anskong@gmail.com>
2025-06-10 13:20:18 +0900

    first committed by gram17, 25/06/10

--- try2.py
+++ try2.py
@@ -1,3 +1,7 @@
 if __name__ == "__main__":
     import langchain_community.document_loaders as loaders
     print(dir(loaders))
+
+# pip install langchain-huggingface sentence-transformers
+# from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+# hf = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
