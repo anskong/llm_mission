cab7f5b5143248ff7bd0dc976b13fa6bcc014070
anskong <anskong@gmail.com>
2025-06-10 16:44:13 +0900

    first committed by gram17, 25/06/10

--- try2.py
+++ try2.py
@@ -1,7 +1,21 @@
-if __name__ == "__main__":
-    import langchain_community.document_loaders as loaders
-    print(dir(loaders))
+from sentence_transformers import SentenceTransformer
+if __name__ == "__main__":	
+    model = SentenceTransformer("jhgan/ko-sroberta-multitask")
+    sentences = [
+        "The weather is lovely today.",
+        "It's so sunny outside!",
+        "He drove to the stadium."
+    ]
+    embeddings = model.encode(sentences)
+
+    similarities = model.similarity(embeddings, embeddings)
+    print(similarities.shape)
+# [3, 3]
+    # import langchain_community.document_loaders as loaders
+    # print(dir(loaders))
 
 # pip install langchain-huggingface sentence-transformers
 # from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 # hf = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
